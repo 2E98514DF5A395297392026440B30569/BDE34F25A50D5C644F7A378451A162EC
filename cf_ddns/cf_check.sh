42317327c68f7d16fdede1b27a6750f854eaf168
2E98514DF5A395297392026440B30569 <118182097+2E98514DF5A395297392026440B30569@users.noreply.github.com>
2023-12-02 21:53:22 +0800

    Update cf_check.sh

--- cf_ddns/cf_check.sh
+++ cf_ddns/cf_check.sh
@@ -9,28 +9,14 @@ informlog="/app/cf_ddns/informlog"
 cf_push="/app/cf_ddns/cf_push.sh"
 
 # 初始化推送
-if [ -e ${informlog} ]; then
-  rm ${informlog}
-fi
+[ -e ${informlog} ] && rm ${informlog}
 
 # 检测是否配置DDNS或更新HOSTS任意一个
-if [[ -z ${dnspod_token} ]]; then
-  IP_TO_DNSPOD=0
-else
-  IP_TO_DNSPOD=1
-fi
+[[ -z ${dnspod_token} ]] && IP_TO_DNSPOD=0 || IP_TO_DNSPOD=1
 
-if [[ -z ${api_key} ]]; then
-  IP_TO_CF=0
-else
-  IP_TO_CF=1
-fi
+[[ -z ${api_key} ]] && IP_TO_CF=0 || IP_TO_CF=1
 
-if [ "$IP_TO_HOSTS" = "true" ]; then
-  IP_TO_HOSTS=1
-else
-  IP_TO_HOSTS=0
-fi
+[ "$IP_TO_HOSTS" = "true" ] && IP_TO_HOSTS=1 || IP_TO_HOSTS=0
 
 if [ $IP_TO_DNSPOD -eq 1 ] || [ $IP_TO_CF -eq 1 ] || [ $IP_TO_HOSTS -eq 1 ]
 then
